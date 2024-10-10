7d7b191622e3a3fc8a26d0581c70e2766f33c556
村山優依 <murayamayuui@murayamayuuis-MacBook-Pro.local>
2024-10-10 11:34:07 -0700

    removed comment

--- mobile/components/Users/Record.tsx
+++ mobile/components/Users/Record.tsx
@@ -41,119 +41,61 @@ const Record = ({route, navigation}: any) => {
   const validateInput = (type: string, datetime: Date) => {
     const s = moment(start);
     const e = moment(end);
+    console.log('start', start, 'end', end)
 
-    console.log('datetime', datetime, 'start', start, 'end', end)
+    if (!start && !end) return true;
     if (type === 'start') {
       const duration = e.diff(moment(datetime), 'hours');
       if (duration < 1) {
         alertError('Start time has to occur before end time');
         return false;
       }
-    } 
+    }
     if (type === 'end') {
-      console.log('here')
       const duration = moment(datetime).diff(s, 'hours');
-      console.log(duration)
-      if (duration < 1) {
+      if (duration < 0) {
         alertError('End time has to occur after start time');
         return false;
       }
     }
-    // const date1 = moment(start);
-    // const date2 = moment(end);
-    // const date3 = moment(date);
-
-    // if (date3.format() > moment().format()) {
-    //   alertError('Cannot set future time');
-    //   return false;
-    // }
-    // console.log(
-    //   'date',
-    //   date,
-    //   'start',
-    //   moment(start).format(),
-    //   'end',
-    //   moment(end).format(),
-    // );
-
-    // const res = date2.diff(date3, 'hours');
-    // console.log(`Hours difference: ${res}`);
-
-    // if (type === 'start') {
-    //   const duration = moment(date3).diff(date2, 'hours');
-    //   console.log('duration', duration);
-    //   if (duration < 1) {
-    //     alertError('Start time has to occur before end time');
-    //     return false;
-    //   }
-    // } else if (type === 'end') {
-    //   const duration = moment(date3).diff(moment(date1), 'hours');
-    //   console.log('duration', duration);
-    //   if (duration < 1) {
-    //     alertError('End time has to occur after start time');
-    //     return false;
-    //   }
-    // }
-
-    // console.log('duration', duration, 'hours', hours)
-    // if (!start || !end) return true
-    // if (
-    //   type === 'start' &&
-    //   new Date(end).getHours() - new Date(date).getHours() < 1
-    // ) {
-    //   setInputError({
-    //     type: 'INVALID_TIME',
-    //     msg: 'Invalid time',
-    //   });
-    //   return false;
-    // } else if (
-    //   type === 'end' &&
-    //   new Date(date).getHours() - new Date(start).getHours() < 1
-    // ) {
-    //   setInputError({
-    //     type: 'INVALID_TIME',
-    //     msg: 'Invalid time',
-    //   });
-    //   return false;
-    // }
     return true;
   };
 
-  const checkDuplicate = (type: string, datetime: Date) => {    
+  const onConfirm = (type: string, datetime: Date) => {
     if (!validateInput(type, datetime)) return;
-    // const x = new Date(date);
-    // const y = new Date(time);
-    // const datetime = new Date(Date.UTC(
-    //   x.getUTCFullYear(),
-    //   x.getUTCMonth(),
-    //   x.getUTCDate(),
-    //   y.getUTCHours(),
-    //   y.getUTCMinutes(),
-    //   y.getUTCSeconds(),
-    // ));
-    // // const datetime = new Date(
-    // //   `${x.getFullYear()}-${
-    // //     x.getMonth() + 1
-    // //   }-${x.getDate()}T${y.getHours()}:${y.getMinutes()}:${y.getSeconds()}Z`,
-    // // );
-    // console.log('final date', datetime);
+    axios
+      .get(`${LOCAL_HOST_URL}/record/day`, {
+        params: {
+          date: datetime,
+          epEmail: email_address,
+          spEmail: serviceProviderEmail,
+        },
+      })
+      .then((res: any) => {
+        console.log('res.data', res.data)
+        res.data.length ? setStart(res.data[0].start_time) : setStart(null);
+        res.data.length ? setEnd(res.data[0].end_time) : setEnd(null);
 
-    // if (!validateInput(type, datetime)) return;
-    // axios
-    //   .get(`${LOCAL_HOST_URL}/record/duplicate`, {
-    //     params: {
-    //       type,
-    //       date: datetime,
-    //       epEmail: email_address,
-    //       spEmail: serviceProviderEmail,
-    //     },
-    //   })
-    //   .then((res: any) => {
-    //     alertAdd(type, time);
-    //   })
-    //   .catch(err => {
-    //     alertDuplicate(type, time);
-    //   });
+        if (isDuplicated(type, res.data)) {
+          alertDuplicate(type, datetime);
+        } else {
+          alertAdd(type, datetime);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const isDuplicated = (type: string, data: any) => {
+    if (!data.length) return false;
+    if (type === 'start' && data[0].start_time !== null) {
+      return true;
+    }
+    if (type === 'end' && data[0].end_time !== null) {
+      return true;
+    }
+    return false;
   };
 
   const alertAdd = (type: string, date: Date) => {
@@ -168,7 +110,7 @@ const Record = ({route, navigation}: any) => {
         },
         {
           text: 'No',
-          onPress: () => console.log('Cancel Pressed'),
+          onPress: () => null,
           style: 'cancel',
         },
       ],
@@ -205,35 +147,10 @@ const Record = ({route, navigation}: any) => {
         mode,
       })
       .then(res => {
-        console.log('res', res.data);
+        console.log('record time', res.data);
         type === 'start' ? setStart(res.data) : setEnd(res.data);
       })
       .catch(err => {});
-  };
-
-  const dateConfirm = (d: Date) => {
-    setDate(d);
-    axios
-      .get(`${LOCAL_HOST_URL}/record/day`, {
-        params: {
-          date: d,
-          epEmail: email_address,
-          spEmail: serviceProviderEmail,
-        },
-      })
-      .then((res: any) => {
-        console.log('data', res.data);
-        if (res.data.length > 0) {
-          setStart(res.data[0].start_time);
-          setEnd(res.data[0].end_time);
-        } else {
-          setStart(null);
-          setEnd(null);
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
   };
 
   const Separator = () => <View style={styles.separator}></View>;
@@ -254,30 +171,6 @@ const Record = ({route, navigation}: any) => {
           Employer: {first_name} {last_name}
         </Text>
       </View>
-      {/* <View>
-        <TouchableOpacity
-          style={{flexDirection: 'row'}}
-          onPress={() => setDateOpen(!dateOpen)}>
-          <Text style={[styles.subHeader, {textDecorationLine: 'underline'}]}>
-            {`Record date: ${moment(date).format('MM/DD/YYYY')}`}
-          </Text>
-          <MaterialIcons name="arrow-drop-down" size={36} color="#000" />
-        </TouchableOpacity>
-      </View> */}
-      {/* <View>
-        <DatePicker
-          modal
-          open={dateOpen}
-          mode="date"
-          date={date}
-          onConfirm={d => dateConfirm(d)}
-          onCancel={() => {
-            setDateOpen(false);
-          }}
-          minimumDate={new Date('2000-01-01')}
-          maximumDate={new Date()}
-        />
-      </View> */}
       <View>
         <View style={styles.dateContainer}>
           <TouchableOpacity
@@ -303,10 +196,9 @@ const Record = ({route, navigation}: any) => {
           <DatePicker
             modal
             open={startOpen}
-            // mode="time"
-            mode='datetime'
+            mode="datetime"
             date={new Date()}
-            onConfirm={d => checkDuplicate('start', d)}
+            onConfirm={d => onConfirm('start', d)}
             onCancel={() => {
               setStartOpen(false);
             }}
@@ -316,10 +208,9 @@ const Record = ({route, navigation}: any) => {
           <DatePicker
             modal
             open={endOpen}
-            // mode="time"
-            mode='datetime'
+            mode="datetime"
             date={new Date()}
-            onConfirm={d => checkDuplicate('end', d)}
+            onConfirm={d => onConfirm('end', d)}
             onCancel={() => {
               setEndOpen(false);
             }}
@@ -329,16 +220,15 @@ const Record = ({route, navigation}: any) => {
         </View>
       </View>
       <View style={styles.todayRecordContainer}>
-      <View>
-        <TouchableOpacity
-          style={{flexDirection: 'row'}}
-          onPress={() => setDateOpen(!dateOpen)}>
-          <Text style={styles.subHeader}>
-            {`Record date: ${moment(date).format('MM/DD/YYYY')}`}
-          </Text>
-          {/* <MaterialIcons name="arrow-drop-down" size={36} color="#000" /> */}
-        </TouchableOpacity>
-      </View>
+        <View>
+          <TouchableOpacity
+            style={{flexDirection: 'row'}}
+            onPress={() => setDateOpen(!dateOpen)}>
+            <Text style={styles.subHeader}>
+              {`Selected date: ${moment(start).format('MM/DD/YYYY')}`}
+            </Text>
+          </TouchableOpacity>
+        </View>
         <View style={styles.todayRecord}>
           <Text>Start</Text>
           <Text>{start ? moment(start).format('LT') : 'Not registered'}</Text>
