7284db945876338a486a772efb132b8dc11286e5
村山優依 <murayamayuui@murayamayuuis-MacBook-Pro.local>
2025-02-24 23:18:46 -0800

    implemented button

--- mobile/components/Record/RecordHistory.tsx
+++ mobile/components/Record/RecordHistory.tsx
@@ -1,19 +1,38 @@
 import React, {useState} from 'react';
-import {Text, ScrollView} from 'react-native';
+import {Text, ScrollView, View} from 'react-native';
 import {TextStyle} from '../../styles';
 import {Record} from '../../swagger';
-import {TopContainer, Separator, AlignContainer} from '../index';
+import {TopContainer, Separator, AlignContainer, Button} from '../index';
 import WorkingHistoryList from '../ServiceProvider/WorkingHistoryList';
 import SearchField from './SearchField';
+import {COLORS} from '../../styles/theme';
 
 const RecordHistory = ({route}: any) => {
   const [records, setRecords] = useState<Record[]>([]);
-
   let centerText = TextStyle.createCenterTextStyle();
+  const buttonStyle = {
+    width: 60,
+    height: 24,
+    backgroundColor: COLORS.BLUE,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 20,
+  };
+
+  const editRecord = () => {};
+
+  const deleteRecord = () => {};
 
   return (
     <TopContainer>
       <SearchField setRecords={setRecords} employer={route.params.employer} />
+      {records?.length !== 0 && (
+        <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
+          <Button title="Edit" style={buttonStyle} onPress={editRecord} />
+          <Button title="Delete" style={buttonStyle} onPress={deleteRecord} />
+        </View>
+      )}
       <ScrollView>
         <AlignContainer>
           <Text>Date</Text>
@@ -24,7 +43,7 @@ const RecordHistory = ({route}: any) => {
         <Separator />
         {records?.length ? (
           records.map((record: Record, index: number) => {
-            return <WorkingHistoryList key={index} record={record}/>;
+            return <WorkingHistoryList key={index} record={record} />;
           })
         ) : (
           <Text style={centerText}>No records matched</Text>
