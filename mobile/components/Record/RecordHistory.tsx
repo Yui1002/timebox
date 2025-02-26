3e2cbbc68bbdc5c43e13e27a6ca4c839a5cc4c61
村山優依 <murayamayuui@murayamayuuis-MacBook-Pro.local>
2025-02-26 11:22:25 -0800

    working on edit record api

--- mobile/components/Record/RecordHistory.tsx
+++ mobile/components/Record/RecordHistory.tsx
@@ -1,30 +1,50 @@
 import React, {useState} from 'react';
 import {Text, ScrollView, View} from 'react-native';
 import {TextStyle} from '../../styles';
-import {Record} from '../../swagger';
+import {Record, DefaultApiFactory} from '../../swagger';
 import {TopContainer, Separator, AlignContainer, Button} from '../index';
 import WorkingHistoryList from '../ServiceProvider/WorkingHistoryList';
 import SearchField from './SearchField';
 import {COLORS} from '../../styles/theme';
+let api = DefaultApiFactory();
 
 const RecordHistory = ({route}: any) => {
   const [records, setRecords] = useState<Record[]>([]);
+  const [rowSelected, setRowSelected] = useState({
+    selectMode: false,
+    selectRow: ''
+  });
   const [editSelected, setEditSelected] = useState({
     editMode: false,
     editRow: ''
   });
+  const [deleteSelected, setDeleteSelected] = useState({
+    deleteMode: false,
+    deleteRow: ''
+  })
   let centerText = TextStyle.createCenterTextStyle();
   const buttonStyle = {
     width: 60,
     height: 24,
-    backgroundColor: editSelected.editMode ? COLORS.BLUE : COLORS.LIGHT_GREY,
+    backgroundColor: rowSelected.selectMode ? COLORS.BLUE : COLORS.LIGHT_GREY,
     borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
     marginLeft: 20,
   };
 
-  const editRecord = () => {};
+  const editRecord = async () => {
+    console.log('editSelected', rowSelected)
+    setEditSelected({
+      editMode: true,
+      editRow: rowSelected.selectRow
+    })
+    try {
+
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   const deleteRecord = () => {};
 
@@ -51,7 +71,9 @@ const RecordHistory = ({route}: any) => {
               <WorkingHistoryList
                 key={index}
                 record={record}
+                rowSelected={rowSelected}
                 editSelected={editSelected}
+                setRowSelected={setRowSelected}
                 setEditSelected={setEditSelected}
               />
             );
--- mobile/components/ServiceProvider/WorkingHistoryList.tsx
+++ mobile/components/ServiceProvider/WorkingHistoryList.tsx
@@ -1,33 +1,132 @@
-import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
 import {getDiff} from '../../helper/momentHelper';
-import {Record} from '../../swagger';
-import {AlignContainer} from '../index';
-import { COLORS } from '../../styles/theme';
+import {Record, TimeType} from '../../swagger';
+import {AlignContainer, DatePickerDropdown, Icon} from '../index';
+import {COLORS} from '../../styles/theme';
+import { ResultModel } from '../../types';
+import { ErrMsg, StatusModel } from '../../enums';
 
 interface WorkingHistoryListProps {
   record: Record;
+  rowSelected: any;
   editSelected: any;
-  setEditSelected: any
+  setRowSelected: any;
+  setEditSelected;
 }
 
-const WorkingHistoryList = ({record, editSelected, setEditSelected}: WorkingHistoryListProps) => {
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const WorkingHistoryList = ({
+  record,
+  rowSelected,
+  editSelected,
+  setRowSelected,
+  setEditSelected,
+}: WorkingHistoryListProps) => {
   const {startTime, endTime} = record;
+  const [startOpen, setStartOpen] = useState<boolean>(false);
+  const [endOpen, setEndOpen] = useState<boolean>(false);
+  const [start, setStart] = useState<string>(startTime ? new Date(startTime).momentFormat('LT') : 'N/A')
+  const [end, setEnd] = useState<string>(startTime ? new Date(startTime).momentFormat('LT') : 'N/A')
+  const [result, setResult] = useState<ResultModel>({
+    status: StatusModel.NULL,
+    message: ''
+  })
+  const date = startTime
+    ? new Date(startTime).momentFormat('YYYY/MM/DD')
+    : 'N/A';
+
   const total = getDiff({start: startTime, end: endTime});
+  const rowStyle = {
+    backgroundColor:
+      JSON.stringify(record) === JSON.stringify(rowSelected.selectRow) &&
+      rowSelected.selectMode
+        ? COLORS.BLUE
+        : COLORS.LIGHT_GREY,
+  };
+
+  const onRowSelect = () => {
+    setRowSelected({
+      selectMode: true,
+      selectRow: record,
+    });
+  };
+
+  const onRowEdit = (time: Date, type: TimeType) => {
+    console.log('time', time);
+    if (type === TimeType.Start) {
+      setStart(time.momentFormat('LT'));
+    } else {
+      setEnd(time.momentFormat('LT'));
+    }
+  };
+
+  const validateInput = () => {
+
+  }
+
+  const editRecord = async () => {
+    try {
+
+    } catch (e) {
+      setResult({
+        status: StatusModel.ERROR,
+        message: 
+      })
+    }
+  }
+
+  const deleteRecord = () => {
+
+  }
 
   return (
-    <TouchableOpacity onPress={() => setEditSelected({
-      editMode: !editSelected.editMode,
-      editRow: record
-    })} style={{backgroundColor: (JSON.stringify(record) === JSON.stringify(editSelected.editRow)) ? COLORS.BLUE : COLORS.LIGHT_GREY}}>
+    <TouchableOpacity onPress={onRowSelect} style={rowStyle}>
       <AlignContainer>
+        <Text>{date}</Text>
         <Text>
-          {startTime ? new Date(startTime).momentFormat('YYYY/MM/DD') : 'N/A'}
+          {editSelected.editMode ? (
+            <TouchableOpacity
+              style={style.container}
+              onPress={() => setStartOpen(!startOpen)}>
+              <Text>{start}</Text>
+              <Icon name="arrow-drop-down" size={20} type='Material'/>
+            </TouchableOpacity>
+          ) : (
+            <Text>{start}</Text>
+          )}
         </Text>
+        <DatePickerDropdown
+          mode="time"
+          open={startOpen}
+          onConfirm={t => onRowEdit(t, TimeType.Start)}
+          onCancel={() => setStartOpen(false)}
+        />
         <Text>
-          {startTime ? new Date(startTime).momentFormat('LT') : 'N/A'}
+          {editSelected.editMode ? (
+            <TouchableOpacity
+              style={style.container}
+              onPress={() => setEndOpen(!endOpen)}>
+              <Text>{end}</Text>
+              <Icon name="arrow-drop-down" size={20} type='Material'/>
+            </TouchableOpacity>
+          ) : (
+            <Text>{end}</Text>
+          )}
         </Text>
-        <Text>{endTime ? new Date(endTime).momentFormat('LT') : 'N/A'}</Text>
+        <DatePickerDropdown
+          mode="time"
+          open={endOpen}
+          onConfirm={t => onRowEdit(t, TimeType.End)}
+          onCancel={() => setEndOpen(false)}
+        />
         <Text>{total ? `${total}h` : 'N/A'}</Text>
       </AlignContainer>
     </TouchableOpacity>
